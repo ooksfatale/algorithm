7deed18daf2af0f9352002e0c09940a0ba31b1da
swkim <ooksfatale@gmail.com>
2021-09-23 00:41:04 +0900

    자바 문제3 완료

---
+++ src/thirdClass/ex3.java
@@ -0,0 +1,27 @@
+package thirdClass;
+import java.util.*;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int sum = 0;
+        int result = 0;
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+
+        for(int num : arr){
+            sum+=num;
+            result+=sum;
+        }
+
+        System.out.println(result);
+    }
+}
